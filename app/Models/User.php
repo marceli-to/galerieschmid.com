72deacee9f73f796537434930c6a5f3be96a4f1f
Marcel Stadelmann <m@marceli.to>
2023-10-27 15:27:21 +0200

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements FilamentUser
