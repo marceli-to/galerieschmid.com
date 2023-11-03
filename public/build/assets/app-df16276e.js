7e7136bf9023605ae804a50e348f739fdfe9ae06
Marcel Stadelmann <m@marceli.to>
2023-11-03 15:13:10 +0100

    wip

---
+++ public/build/assets/app-df16276e.js
@@ -0,0 +1,1 @@
+(function(){const t=document.querySelector("[data-toggle-privacy]"),e=document.querySelector("[data-privacy]");t&&e&&t.addEventListener("click",()=>{e.classList.toggle("is-hidden")})})();
