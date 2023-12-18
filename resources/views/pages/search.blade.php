cc261f18ef24dc65408bf9f24534a2d43c75f993
Marcel Stadelmann <m@marceli.to>
2023-12-18 20:17:35 +0100

    wip

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -5,17 +5,16 @@
 	<h2>{{ __('Suche') }}</h2>
 
   <x-search.result :hits="$hits" :keywords="$keywords" />
-
-	@foreach ($records['exhibitions'] as $record)
-    <x-search.records.exhibition :record="$record" />
-	@endforeach
-
-	@foreach ($records['artworks'] as $record)
-    <x-search.records.artwork :record="$record" />
-	@endforeach
-  
   @foreach ($records['artists'] as $record)
     <x-search.records.artist :record="$record" />
+  @endforeach
+
+  @foreach ($records['artworks'] as $record)
+    <x-search.records.artwork :record="$record" />
+	@endforeach
+ 
+	@foreach ($records['exhibitions'] as $record)
+    <x-search.records.exhibition :record="$record" />
   @endforeach
 
 </section>
