6c2976c5ff8a0d03e1e3ad26a1985f6a1f77cd9d
Marcel Stadelmann <m@marceli.to>
2024-05-13 09:30:57 +0200

    wip

--- resources/views/components/newsletter/article.blade.php
+++ resources/views/components/newsletter/article.blade.php
@@ -4,13 +4,15 @@
     {{ $article->title }}
   </h2>
   <table class="mb-20">
-    <td class="w-[160px]">
-      @if ($article->media->first())
-        <img src="{{ $article->media->first()->getUrl('newsletter') }}" alt="{{ $article->title }}" class="w-[120px] my-2 mr-10">
-      @endif
-    </td>
-    <td class="align-top">
-      {!! $article->text !!}
-    </td>
+    <tr>
+      <td class="w-[160px]">
+        @if ($article->media->first())
+          <img src="{{ $article->media->first()->getUrl('newsletter') }}" alt="{{ $article->title }}" class="w-[120px] my-2 mr-10">
+        @endif
+      </td>
+      <td class="align-top">
+        {!! $article->text !!}
+      </td>
+    </tr>
   </table>
 </article>
