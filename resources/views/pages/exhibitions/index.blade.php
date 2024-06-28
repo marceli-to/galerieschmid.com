40f11a7a5ef9a2f55540ffe50774f48fe8dd7746
Marcel Stadelmann <m@marceli.to>
2024-06-28 20:02:57 +0200

    wip

--- resources/views/pages/exhibitions/index.blade.php
+++ resources/views/pages/exhibitions/index.blade.php
@@ -2,12 +2,12 @@
 @section('seo_title', 'Ausstellungen')
 @section('content')
 <section class="grid__cell grid__cell--last grid__cell--2_3 grid-order--2">
-  {{-- @if ($exhibitions['current'])
+  @if (isset($exhibitions['current']) && $exhibitions['current'])
     <x-exhibitions.show :exhibition="$exhibitions['current']" />
-  @endif --}}
+  @endif
 </section>
 <section class="grid__cell grid__cell--1_3 grid-order--1">
   <h2>{{ __('Ausstellungen') }}</h2>
-  <x-exhibitions.list :exhibitions="$exhibitions" :activeExhibitionId="$exhibitions['current']->id" />
+  {{-- <x-exhibitions.list :exhibitions="$exhibitions" :activeExhibitionId="$exhibitions['current']->id" /> --}}
 </section>
 @endsection
